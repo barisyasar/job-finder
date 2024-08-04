f374384e88d1e49e51d996878c99f028d6683b62
Barış Yaşar <75898912+barisyasar@users.noreply.github.com>
2024-08-04 19:39:01 +0300

    User register service added

---
+++ src/services/userService.js
@@ -0,0 +1,12 @@
+// User Service
+import axiosInstance from "../config/axiosInstance";
+
+export const registerUser = async (userData) => {
+  try {
+    const response = await axiosInstance.post("/register", userData);
+    return response.data;
+  } catch (error) {
+    console.error("Error registering user:", error);
+    throw error;
+  }
+};
