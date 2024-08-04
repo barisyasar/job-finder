5283c2f9d132f0d11c42ffad8a8c486526bae5f6
Barış Yaşar <75898912+barisyasar@users.noreply.github.com>
2024-08-04 14:18:54 +0300

    Temporary jobs added to visualize

--- src/components/JobPanel/JobItem.jsx
+++ src/components/JobPanel/JobItem.jsx
@@ -10,8 +10,10 @@ import {
 import { BriefcaseBusiness } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "../ui/button";
+import JobDetailDialog from "../JobDetailDialog";
+import { Separator } from "../ui/separator";
 
-function JobItem() {
+function JobItem({ job }) {
   return (
     <Card className="job-item lg:flex items-start">
       <BriefcaseBusiness size={48} className="hidden lg:block ms-6 mt-6" />
@@ -19,28 +21,31 @@ function JobItem() {
         <CardHeader>
           <div className="flex items-center gap-3">
             <BriefcaseBusiness size={28} className="lg:hidden" />
-            <CardTitle>Card Title</CardTitle>
+            <CardTitle className="flex items-center">
+              {job.companyName}
+              <Separator
+                orientation="vertical"
+                className="h-6 mx-2 bg-blue-800"
+              />
+              {job.name}
+            </CardTitle>
           </div>
-          <CardDescription>Location:</CardDescription>
-          <CardDescription>Salary:</CardDescription>
+          <CardDescription>Location: {job.location}</CardDescription>
+          <CardDescription>Salary: {job.salary}</CardDescription>
         </CardHeader>
         <CardContent className="max-w-3xl">
-          <p className="mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut qui
-            iusto magni eius harum, corporis fugiat similique saepe nam
-            temporibus?
-          </p>
+          <p className="mb-4">{job.description}</p>
           <div className="flex gap-2">
-            <Badge className="bg-blue-600 text-white"># ipsum</Badge>
-            <Badge className="bg-blue-600 text-white"># dolar</Badge>
-            <Badge className="bg-blue-600 text-white"># sit</Badge>
+            {job.keywords.map((keyword) => (
+              <Badge key={keyword} className="bg-blue-800 text-white">
+                # {keyword}
+              </Badge>
+            ))}
           </div>
         </CardContent>
       </div>
       <CardFooter className="gap-5 lg:flex lg:flex-col lg:gap-3 lg:m-6">
-        <Button className="w-[80px]" size="sm">
-          Detail
-        </Button>
+        <JobDetailDialog job={job} />
         <Button className="w-[80px]" size="sm" variant="outline">
           Withdraw
         </Button>
