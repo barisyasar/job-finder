22b29f86401a7c4daad40de8b84d61435d945085
Barış Yaşar <75898912+barisyasar@users.noreply.github.com>
2024-08-04 20:22:16 +0300

    Login service added

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,11 +2,11 @@
 import axiosInstance from "../config/axiosInstance";
 
 export const registerUser = async (userData) => {
-  try {
-    const response = await axiosInstance.post("/register", userData);
-    return response.data;
-  } catch (error) {
-    console.error("Error registering user:", error);
-    throw error;
-  }
+  const response = await axiosInstance.post("/register", userData);
+  return response.data;
+};
+
+export const loginUser = async (userData) => {
+  const response = await axiosInstance.post("/login", userData);
+  return response.data;
 };
