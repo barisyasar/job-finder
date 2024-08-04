82ec01a3117efd4f52a97bc73edc256b2fc247be
Barış Yaşar <75898912+barisyasar@users.noreply.github.com>
2024-08-04 21:22:14 +0300

    user profile is now showing

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,14 +1,17 @@
 // User Profile
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import useZustand from "@/state/useZustand";
 
 function UserProfile() {
+  const { user } = useZustand();
+
   return (
     <div className="user-profile">
       <Avatar className="user-img">
-        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-        <AvatarFallback>CN</AvatarFallback>
+        <AvatarImage src={user.profileImage} alt={user.email} />
+        <AvatarFallback>{user.email.charAt(0)}</AvatarFallback>
       </Avatar>
-      <div className="user-email">example@gmail.com</div>
+      <div className="user-email hidden sm:block">{user.email}</div>
     </div>
   );
 }
