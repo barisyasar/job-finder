b060b32bdd0580ed8ed538ec3d21db1167ebd1f7
Barış Yaşar <75898912+barisyasar@users.noreply.github.com>
2024-08-04 19:38:27 +0300

    axios service replaced

---
+++ src/config/axiosInstance.js
@@ -0,0 +1,20 @@
+// Axios Instance
+import useZustand from "@/state/useZustand";
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.VITE_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const { user } = useZustand();
+  if (user?.accessToken) {
+    config.headers.Authorization = `${user.tokenType} ${user.accessToken}`;
+  }
+  return config;
+});
+
+export default axiosInstance;
