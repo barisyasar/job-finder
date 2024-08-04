fbe7c8e6d00a1cd8d967bd40bf08f61d218a16be
Barış Yaşar <75898912+barisyasar@users.noreply.github.com>
2024-08-04 21:31:18 +0300

    auth storage added

--- src/state/useZustand.js
+++ src/state/useZustand.js
@@ -1,33 +1,47 @@
-// useZustand
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useZustand = create((set) => ({
-  // dialogs
-  dialog: null,
-  setDialog: (dialog) => set({ dialog }),
-  resetDialog: () => set({ dialog: null }),
+const useZustand = create(
+  persist(
+    (set) => ({
+      // dialogs
+      dialog: null,
+      setDialog: (dialog) => set({ dialog }),
+      resetDialog: () => set({ dialog: null }),
 
-  // user data
-  user: null,
-  tokenType: null,
-  accessToken: null,
-  refreshToken: null,
-
-  setAuthData: (authData) =>
-    set(() => ({
-      user: authData.user,
-      tokenType: authData.tokenType,
-      accessToken: authData.accessToken,
-      refreshToken: authData.refreshToken,
-    })),
-
-  clearAuthData: () =>
-    set(() => ({
+      // user data (persisted)
       user: null,
       tokenType: null,
       accessToken: null,
       refreshToken: null,
-    })),
-}));
+
+      setAuthData: (authData) =>
+        set(() => ({
+          user: authData.user,
+          tokenType: authData.tokenType,
+          accessToken: authData.accessToken,
+          refreshToken: authData.refreshToken,
+        })),
+
+      clearAuthData: () =>
+        set(() => ({
+          user: null,
+          tokenType: null,
+          accessToken: null,
+          refreshToken: null,
+        })),
+    }),
+    {
+      name: "auth-storage",
+      getStorage: () => localStorage,
+      partialize: (state) => ({
+        user: state.user,
+        tokenType: state.tokenType,
+        accessToken: state.accessToken,
+        refreshToken: state.refreshToken,
+      }),
+    }
+  )
+);
 
 export default useZustand;
