867c6799fbde7f34d9976a26a28163a448c90d03
Barış Yaşar <75898912+barisyasar@users.noreply.github.com>
2024-08-04 19:54:36 +0300

    schemas updated
    According to response of register crated new schema

--- src/config/validations.js
+++ src/config/validations.js
@@ -1,6 +1,16 @@
 import * as Yup from "yup";
 
-const baseSchema = Yup.object().shape({
+const signUpchema = Yup.object().shape({
+  email: Yup.string()
+    .required("Email is required")
+    .email("Invalid email address"),
+
+  password: Yup.string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters long"),
+});
+
+const loginchema = Yup.object().shape({
   email: Yup.string()
     .required("Email is required")
     .email("Invalid email address"),
@@ -8,4 +18,4 @@ const baseSchema = Yup.object().shape({
   password: Yup.string().required("Password is required"),
 });
 
-export default baseSchema;
+export { loginchema, signUpchema };
