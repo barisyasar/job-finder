14f0dda9d2e8944bdf03b830ef6956b0ba5839a4
Barış Yaşar <75898912+barisyasar@users.noreply.github.com>
2024-08-04 19:55:20 +0300

    axios instance base url fixed

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,20 +1,20 @@
 // Axios Instance
-import useZustand from "@/state/useZustand";
+// import useZustand from "@/state/useZustand";
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-axiosInstance.interceptors.request.use((config) => {
+/* axiosInstance.interceptors.request.use((config) => {
   const { user } = useZustand();
   if (user?.accessToken) {
     config.headers.Authorization = `${user.tokenType} ${user.accessToken}`;
   }
   return config;
-});
+}); */
 
 export default axiosInstance;
