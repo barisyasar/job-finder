61ffb55f5830f654e9c2433ee42761d067d7d811
Barış Yaşar <75898912+barisyasar@users.noreply.github.com>
2024-08-04 15:03:27 +0300

    Separatpr added aside layout

--- src/components/AsideLayout/AsideLayout.jsx
+++ src/components/AsideLayout/AsideLayout.jsx
@@ -3,6 +3,8 @@ import Header from "../Header";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
 import useScreen from "@/hooks/useScreen";
+import AppliedJobs from "../AppliedJobs";
+import { Separator } from "../ui/separator";
 
 function AsideLayout() {
   const isAboveLarge = useScreen("lg");
@@ -14,7 +16,15 @@ function AsideLayout() {
           <Header />
           <Outlet />
         </div>
-        {isAboveLarge && <aside>asdf</aside>}
+
+        {isAboveLarge && (
+          <>
+            <Separator orientation="vertical" className="h-screen" />
+            <aside className="w-1/3">
+              <AppliedJobs />
+            </aside>
+          </>
+        )}
       </div>
       <Toaster />
     </>
